eb45979175f0ec43bba75ad08a42a1a4b0970d4c
Olivier Boucher <boucher.oli@gmail.com>
2015-07-20 08:50:06 -0400

    Added websocket support draft

--- routes/routes.go
+++ routes/routes.go
@@ -3,12 +3,42 @@ package routes
 import (
   "net/http"
   "github.com/gorilla/mux"
+  "github.com/gorilla/websocket"
   "github.com/OlivierBoucher/go-tracking-server/middlewares"
 )
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+    CheckOrigin: func(r *http.Request) bool {
+      //TODO: Perform checks on the request and deny/accept
+      return true
+    },
+}
 //Handler for /track route
 //From here we have a valid authentified json request
 func handleTrack(w http.ResponseWriter, r *http.Request) {
   //TODO: Handle the json payload
+}
+//Handler for /connected route
+//Allows websocket connection
+func handleConnected(w http.ResponseWriter, r *http.Request) {
+  conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        //TODO: Handle the error
+        panic(err)
+    }
+    for {
+        messageType, p, err := conn.ReadMessage()
+        if err != nil {
+            return
+        }
+        //TODO: Define a protocol
+        
+        err = conn.WriteMessage(messageType, p);
+        if  err != nil {
+            return
+        }
+    }
 }
 //Handlers - Returns a mux router containing all handlers for all routes
 func Handlers() *mux.Router {
@@ -16,6 +46,8 @@ func Handlers() *mux.Router {
   //Each supported route is being added to the router
   trackHandler := http.HandlerFunc(handleTrack)
   r.Handle("/track", middlewares.EnforceJSONHandler(middlewares.AuthHandler(trackHandler))).Methods("POST", "GET")
+  connectedHandler := http.HandlerFunc(handleConnected)
+  r.Handle("/connected", connectedHandler)
 
   return r
 }
