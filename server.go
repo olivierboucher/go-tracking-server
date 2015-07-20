d2f802698595a94b593e90ed7a2fe82bb8627471
Olivier Boucher <boucher.oli@gmail.com>
2015-07-20 12:25:33 -0400

    Re-enabled db

--- server.go
+++ server.go
@@ -3,19 +3,19 @@ package main
 import (
   "log"
   "net/http"
-  _"database/sql"
+  "database/sql"
   "github.com/OlivierBoucher/go-tracking-server/routes"
   "github.com/OlivierBoucher/go-tracking-server/ctx"
-  _"github.com/OlivierBoucher/go-tracking-server/datastores"
+  "github.com/OlivierBoucher/go-tracking-server/datastores"
 )
 func main() {
-  /*authDb, err := sql.Open("mysql", "")
+  authDb, err := sql.Open("mysql", "")
   if err != nil {
         log.Fatalf("Error on initializing database connection: %s", err.Error())
   }
-  defer authDb.Close()*/
+  defer authDb.Close()
 
-  context := &ctx.Context{/*AuthDb: datastores.NewAuthInstance(authDb)*/}
+  context := &ctx.Context{AuthDb: datastores.NewAuthInstance(authDb)}
 
   log.Fatal(http.ListenAndServe(":1337", routes.Handlers(context)))
 }
